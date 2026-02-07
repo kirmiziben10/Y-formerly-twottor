4e502182d5ee166924ddcc9e5af64c705c6b8b0d
kirmiziben10 <tigiy20032003@gmail.com>
2026-02-08 01:59:58 +0300

    Add Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./index.css";
+import Sidebar from "./components/Sidebar";
 import { Sun, Moon } from "lucide-react";
 
 function App() {
@@ -11,18 +12,24 @@ function App() {
   return (
     <div
       className={
-        "bg-zinc-50 dark:bg-zinc-950 grid place-items-center h-screen w-f " +
+        "bg-zinc-50 dark:bg-zinc-950 text-black  dark:text-white  flex w-full h-screen w-f " +
         theme
       }
     >
-      <div className="bg-zinc-300 dark:bg-zinc-700 flex place-items-center rounded p-1">
-          <button className="p-1 rounded hover:bg-zinc-400" onClick={manageTheme}>
-            {theme === "dark" ? (
-              <Sun color="white" size={24} />
-            ) : (
-              <Moon color="black" size={24} />
-            )}
-          </button>
+      <Sidebar className/>
+      <div className=" flex justify-center place-items-center flex-1">
+          TESTING
+      </div>
+
+      {/*dark-mode toggle*/}
+      <div className="bg-zinc-300 dark:bg-zinc-700 bottom-2 right-2 absolute flex place-items-center rounded p-1">
+        <button className="p-1 rounded hover:bg-zinc-400" onClick={manageTheme}>
+          {theme === "dark" ? (
+            <Sun color="white" size={24} />
+          ) : (
+            <Moon color="black" size={24} />
+          )}
+        </button>
       </div>
     </div>
   );
