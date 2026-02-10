0ee8e447e20d3a23e118fdd1aeaad2c3add8dec8
kirmiziben10 <tigiy20032003@gmail.com>
2026-02-10 18:05:25 +0300

    Fix broken css on Post component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -8,7 +8,7 @@ import type { Tweet } from "../tweets.ts"
 
 export default function Feed() {
   return (
-    <main className="flex flex-row space-between content-between w-262.5">
+    <main className="flex flex-row space-between content-between w-262.5 overflow-scroll">
       <div className="flex flex-col w-150 border-zinc-700 border-x">
         <div className="flex flex-row items-center justify-evenly w-full h-13.5 border-zinc-700 border-b shrink-0">
           <div className="flex w-1/2 items-center justify-center h-full border-zinc-700">
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -8,12 +8,12 @@ export default function Post({tweet}: {tweet: Tweet}) {
   const user = users.find(user => user.id === tweet.user_id)
   return (
     <div className="w-149.5 h-fit flex border-zinc-700 border-b">
-      <div className="flex place-items-center size-15 ml-2">
-        <a href={user?.handle}><img className="bg-white dark:bg-black size-11 object-contain rounded-full" src={user?.avatar} alt="" /></a>
+      <div className="flex items-start justify-center w-15 shrink-0">
+        <a href={user?.handle}><img className="bg-white dark:bg-black size-11.5 mt-2.5 object-contain rounded-full" src={user?.avatar} alt="" /></a>
       </div>
       <div>
         <div className="mt-2.5 ml-2">{user?.name} <span className="opacity-50">{user?.handle}</span></div>
-        <div className="m-2">{tweet.content}</div>
+        <div className="flex wrap-break-word w-full grow-0 pt-1 pb-0 p-2">{tweet.content}</div>
         <div className="flex flex-row items-center justify-start h-10"><div className="flex flex-row justify-end p-2 m-1"><Heart/><span>{tweet.likes}</span></div> <div className="flex flex-row justify-center p-2"><Repeat2/><span>{tweet.retweets}</span></div></div>
       </div>
     </div>
